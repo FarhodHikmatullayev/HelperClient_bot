f300f2750705a07dd4d8d0a5a2d8258bdc59c3c8
Farhod <farhodjonhikmatullayev@gmail.com>
2024-08-12 14:44:50 +0500

    modified

--- data/config.py
+++ data/config.py
@@ -7,9 +7,14 @@ env = Env()
 env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-SUPERADMINS = env.list("SUPERADMINS")  # adminlar ro'yxati
+# BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+# ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+# SUPERADMINS = env.list("SUPERADMINS")  # adminlar ro'yxati
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # Bot toekn
+ADMINS = os.getenv("ADMINS")  # adminlar ro'yxati
+SUPERADMINS = os.getenv("SUPERADMINS")  # adminlar ro'yxati
+
 # IP = env.str("ip")  # Xosting ip manzili
 IP = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 

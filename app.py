8d19b74de142486b83693d628032272202b15722
Farhod <farhodjonhikmatullayev@gmail.com>
2024-08-13 16:47:17 +0500

    skip_updates is added

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ async def on_startup(dispatcher):
     await set_default_commands(dispatcher)
 
     # Bot ishga tushgani haqida adminga xabar berish
+    # await on_startup_notify(dispatcher)
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, on_startup=on_startup)
+    executor.start_polling(dp, on_startup=on_startup, skip_updates=True)

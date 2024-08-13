e15b750f589e832728f7516636a39c9d98b1fde8
Farhod <farhodjonhikmatullayev@gmail.com>
2024-08-13 14:30:36 +0500

    admin url was changed

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('farhod/', admin.site.urls),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

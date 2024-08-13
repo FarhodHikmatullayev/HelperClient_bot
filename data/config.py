ebb69931b7e8f88b411eb948f7d44f4fdbd9e243
Farhod <farhodjonhikmatullayev@gmail.com>
2024-08-13 14:57:59 +0500

    ip was deleted

--- data/config.py
+++ data/config.py
@@ -8,7 +8,7 @@ env.read_env()
 BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
 ADMINS = env.list("ADMINS")  # adminlar ro'yxati
 SUPERADMINS = env.list("SUPERADMINS")  # adminlar ro'yxati
-IP = env.str("ip")  # Xosting ip manzili
+# IP = env.str("ip")  # Xosting ip manzili
 
 # DB_USER = env.str("DB_USER")
 # DB_PASS = env.str("DB_PASS")

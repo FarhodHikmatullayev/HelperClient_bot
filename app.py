c55da7272202424e8ad2f9bb3ba20c2c99a6ccb7
Farhod <farhodjonhikmatullayev@gmail.com>
2024-08-13 19:41:12 +0500

    webhook is added

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from aiogram import executor
 
-from loader import dp, db
+from loader import dp, db, bot
 import middlewares, filters, handlers
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
 
 
 async def on_startup(dispatcher):
+    await set_webhook()
     await db.create()
     # await db.drop_users()
 
@@ -15,6 +16,11 @@ async def on_startup(dispatcher):
 
     # Bot ishga tushgani haqida adminga xabar berish
     # await on_startup_notify(dispatcher)
+
+
+async def set_webhook():
+    webhook_url = f'https://helperclient-bot.onrender.com/webhook'
+    await bot.set_webhook(webhook_url)
 
 
 if __name__ == '__main__':

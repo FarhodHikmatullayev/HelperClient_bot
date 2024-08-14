fa20da12540e2c78364e4c7ff37d327bfd5b67d7
Farhod <farhodjonhikmatullayev@gmail.com>
2024-08-14 12:32:14 +0500

    modified

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from aiogram import executor
-from aiogram.utils.exceptions import TerminatedByOtherGetUpdates
 
 from loader import dp, db, bot
 import middlewares, filters, handlers
@@ -29,16 +28,6 @@ async def on_startup(dispatcher):
 #     webhook_url = f'https://helperclient-bot.onrender.com/webhook'
 #     await bot.set_webhook(webhook_url)
 
-#
-# if __name__ == '__main__':
-#     executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
 
 if __name__ == '__main__':
-    try:
-        executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
-    except TerminatedByOtherGetUpdates as e:
-        print(f"Error: {e}")
-        print("Another instance of the bot is running. Stopping the current instance.")
-    except Exception as e:
-        print(f"Unexpected error: {e}")
-        raise e
+    executor.start_polling(dp, on_startup=on_startup)
--- keep_alive.py
+++ keep_alive.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def run():
-    app.run(host='0.0.0.0', port=52094)
+    app.run(host='0.0.0.0', port=10000, debug=False)
 
 
 def keep_alive():

a2f62aaeacce498a466ff7751982decccbfebecb
Farhod <farhodjonhikmatullayev@gmail.com>
2024-08-14 12:14:19 +0500

    modified

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from aiogram import executor
+from aiogram.utils.exceptions import TerminatedByOtherGetUpdates
 
 from loader import dp, db, bot
 import middlewares, filters, handlers
@@ -28,6 +29,16 @@ async def on_startup(dispatcher):
 #     webhook_url = f'https://helperclient-bot.onrender.com/webhook'
 #     await bot.set_webhook(webhook_url)
 
+#
+# if __name__ == '__main__':
+#     executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
 
 if __name__ == '__main__':
-    executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
+    try:
+        executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
+    except TerminatedByOtherGetUpdates as e:
+        print(f"Error: {e}")
+        print("Another instance of the bot is running. Stopping the current instance.")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        raise e
